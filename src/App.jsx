731d0ed46eaf0be854e8ff15efb6491a99220643
Renan <renansalmazio@gmail.com>
2023-08-19 18:30:35 -0300

    arquivos do projeto

---
+++ src/App.jsx
@@ -0,0 +1,147 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const emptyBoard = Array(9).fill('')
+  const [board, setBoard] = useState(emptyBoard)
+  const [score1, setScore1] = useState(0)
+  const [score2, setScore2] = useState(0)
+  const [currentPlayer, setCurrentPlayer] = useState('O')
+  const [winner, setWinner] = useState(null)
+
+
+  const resetGame = () => {
+    setBoard(emptyBoard)
+    setCurrentPlayer('O')
+    setWinner(null)
+  }
+
+ 
+  const updateScore = (winner) => {
+    if(winner === "E") return
+    if(winner === "O") { 
+       let scorePlayer1  = score1 + 1
+       setScore1(scorePlayer1)
+      }
+
+      if(winner === "X") { 
+        let scorePlayer2  = score2 + 1 
+        setScore2(scorePlayer2)
+       }
+ 
+  }
+
+   const handleCellClick = (index) => {
+     if(winner) return null
+
+
+     if(board[index] !== "")return null
+     
+        
+
+
+    setBoard(board
+      .map((item,itemIndex) => 
+        itemIndex === index ? currentPlayer : item
+      )
+      )
+     setCurrentPlayer(currentPlayer === "X" ? "O" : "X")
+   }
+   
+  const  checkWinner = () =>{
+    const possibleWaysToWin =
+    [ 
+      //horinzontall way
+     [board[0],board[1],board[2]],
+     [board[3],board[4],board[5]],
+     [board[6],board[7],board[8]],
+
+     // verticall way 
+
+     [board[0],board[3],board[6]],
+     [board[1],board[4],board[7]],
+     [board[2],board[5],board[8]],
+
+     // diagonall way
+      
+     [board[0],board[4],board[8]],
+     [board[2],board[4],board[6]],
+
+
+    ]   
+    
+    possibleWaysToWin.forEach(cells => {
+      if(cells.every(cell => cell === 'O' )){
+         setWinner("O") 
+         updateScore("O")
+      }
+      if(cells.every(cell => cell === 'X' )){
+        setWinner("X");
+        updateScore("X")
+      }
+
+    })
+   
+    checkDraw()
+  }
+
+
+ const checkDraw = () => {
+  if(board.every(item => item !== "")) 
+  setWinner("E")
+ }
+
+  useEffect(checkWinner,[board])
+  return (
+    <>
+    <main>
+     <h1 className='title'>Jogo da Velha</h1>
+     <div className="score-box">
+     <h2>Score</h2>
+     <div className="divider">
+     <span> Player O  {score1}</span>
+     <span> Player X  {score2}</span>
+     </div>
+     </div>
+     
+     <div className={
+      `board ${winner ? "game-over" : ""}`
+    }>
+      {board.map((item, index)=>(
+             
+             <div 
+             key={index} 
+             className={`cell ${item}`}
+             onClick={() => handleCellClick(index)}
+             
+             >
+              {item}
+              
+            </div>
+
+
+
+      ))}
+    
+      
+     </div>
+
+
+    {winner &&
+     <footer>
+      {winner === "E" ? 
+      <h2 className='winner-message'><span className={winner}>Empatou</span></h2>
+     : 
+     <h2 className='winner-message'><span className={winner}>{winner}</span></h2>
+    }
+     <button onClick={resetGame}>Recomeçar o Jogo </button>
+     </footer>
+}
+    </main>
+  </>
+  )
+}
+
+export default App
